8793d75de25e3e0d256ec296e994256f158d5fa0
limweekit <e1406546@u.nus.edu>
2025-08-22 02:10:28 +0800

    Edited name and greet/exit message

---
+++ src/main/java/YapGPT.java
@@ -0,0 +1,14 @@
+public class YapGPT {
+    public static void main(String[] args) {
+        String divider = "___________________________________________\n";
+        String welcomeMessage =
+                  divider
+                + "Hello! I'm YapGPT, your favourite chatbot. \n"
+                + "What can I do for you? \n"
+                + divider
+                + "Bye for now. Hope to see you again soon! \n"
+                + divider;
+
+        System.out.println(welcomeMessage);
+    }
+}
