0f2a0f67d64a33eafd7e6dcb8aa31e3d652242ed
limweekit <e1406546@u.nus.edu>
2025-08-22 02:35:21 +0800

    Added echo function

--- src/main/java/YapGPT.java
+++ src/main/java/YapGPT.java
@@ -1,14 +1,34 @@
+import java.util.Scanner;
+
 public class YapGPT {
     public static void main(String[] args) {
-        String divider = "___________________________________________\n";
+        Scanner sc = new Scanner(System.in);
+        String divider = "___________________________________________\n"; // 43 underscores
         String welcomeMessage =
                   divider
                 + "Hello! I'm YapGPT, your favourite chatbot. \n"
                 + "What can I do for you? \n"
-                + divider
-                + "Bye for now. Hope to see you again soon! \n"
+                + divider;
+
+        String goodbyeMessage =
+                divider
+                + "Bye! Hope to see you again soon! \n"
                 + divider;
 
         System.out.println(welcomeMessage);
+
+        while (true) {
+            // Small customization to make the chatbot act like a messaging app
+            System.out.print("You: ");
+            String input = sc.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println(goodbyeMessage);
+                break;
+            }
+            System.out.println("YapGPT: " + input);
+        }
+        sc.close();
     }
 }
+
