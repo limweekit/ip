b577093738b7285ec67ae9971eac97882c18793f
limweekit <e1406546@u.nus.edu>
2025-08-22 10:27:02 +0800

    Added list functionality

--- src/main/java/YapGPT.java
+++ src/main/java/YapGPT.java
@@ -1,32 +1,59 @@
 import java.util.Scanner;
 
 public class YapGPT {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+
+    private static void boxPrint(String message) {
         String divider = "___________________________________________\n"; // 43 underscores
-        String welcomeMessage =
-                  divider
-                + "Hello! I'm YapGPT, your favourite chatbot. \n"
-                + "What can I do for you? \n"
-                + divider;
+        System.out.println(divider + message + "\n" + divider);
+    }
+    public static void main(String[] args) {
+        final String welcomeMessage =
+                "Hello! I'm YapGPT, your favourite chatbot. \n"
+                + "What can I do for you?";
 
-        String goodbyeMessage =
-                divider
-                + "Bye! Hope to see you again soon! \n"
-                + divider;
+        final String goodbyeMessage =
+                "Bye! Hope to see you again soon!";
 
-        System.out.println(welcomeMessage);
+        String[] list = new String[100];
+        int count = 0;
+        Scanner sc = new Scanner(System.in);
+
+        boxPrint(welcomeMessage);
 
         while (true) {
             // Small customization to make the chatbot act like a messaging app
             System.out.print("You: ");
+            if (!sc.hasNextLine()) {
+                boxPrint(goodbyeMessage);
+                break;
+            }
+
             String input = sc.nextLine();
 
             if (input.equals("bye")) {
-                System.out.println(goodbyeMessage);
+                boxPrint(goodbyeMessage);
                 break;
             }
-            System.out.println("YapGPT: " + input);
+
+            if (input.equals("list")) {
+                if (count == 0) {
+                    boxPrint("No items added yet.");
+                } else {
+                    StringBuilder sb = new StringBuilder();
+                    for (int i = 0; i < count; i++) {
+                        sb.append(i + 1).append(": ").append(list[i]).append("\n");
+                    }
+                    boxPrint(sb.toString());
+                }
+                continue;
+            }
+
+            if (count >= list.length) {
+                boxPrint("Sorry, list is full.");
+            } else {
+                list[count++] = input;
+                boxPrint("Added: " + input);
+            }
         }
         sc.close();
     }
