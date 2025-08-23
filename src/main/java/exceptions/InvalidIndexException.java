f90ab376addb3108f9a4d25534f9a5ead83003b9
limweekit <e1406546@u.nus.edu>
2025-08-24 00:55:24 +0800

    Utilized enum for TaskTypes

--- src/main/java/exceptions/InvalidIndexException.java
+++ src/main/java/exceptions/InvalidIndexException.java
@@ -4,6 +4,6 @@ public class InvalidIndexException extends YapGPTException {
     public InvalidIndexException(String action, int max) {
         super(max == 0
                 ? "Uh uh, no item in the list to " + action + "."
-                : "Invalid task number to " + action + ". There are only " + max + " items in the list.");
+                : "Invalid task number to " + action + ". Valid range: 1 to " + max + ".");
     }
 }
